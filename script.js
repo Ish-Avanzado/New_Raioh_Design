e545d36a19e5a615993dc36ff643544797eb8aa0
brilyantes <isiaavanzado@gmail.com>
2025-03-11 13:24:40 +0800

    updating files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// togglemenu
 const burgir = document.querySelector(".toggle_menu");
 const hNav = document.querySelector(".header_nav");
 burgir.addEventListener("click", () => {
@@ -5,6 +7,8 @@ burgir.addEventListener("click", () => {
     hNav.classList.toggle("open");
 });
 
+
+// arrowup
 const ulo = document.querySelector(".header");
 const taas = document.querySelector(".backToTop");
 window.addEventListener("scroll", () => {
@@ -17,6 +21,41 @@ window.addEventListener("scroll", () => {
         taas.classList.remove("up");
     }
 });
+
+
+// dropdown
+const tanong = document.querySelectorAll(".faq_item h4");
+tanong.forEach((question) => {
+    question.addEventListener("click", () => {
+        question.querySelector("i").classList.toggle("open");
+        question.nextElementSibling.classList.toggle("open");
+    });
+});
+
+
+
+const navLink = document.querySelectorAll(".tabs_nav_link");
+const navContent = document.querySelectorAll(".tabs_item");
+navLink.forEach((nav) => {
+    nav.addEventListener("click", () => {
+        removeActiveNav();
+        nav.classList.add("active");
+        const activeContent = document.querySelector(`#${nav.id}-content`);
+        removeActiveContent();
+        activeContent.classList.add("active");
+    });
+});
+
+function removeActiveNav() {
+    navLink.forEach((nav) => {
+        nav.classList.remove("active");
+    });
+}
+function removeActiveContent() {
+    navContent.forEach((content) => {
+        content.classList.remove("active");
+    });
+}
 
 
 // const abtImg = document.querySelectorAll(".about_img img")
